9fc1bd45c8dace6ab4e3be43f2545a0e1cb7c4c7
Francesco <med.dama@gmail.com>
2020-05-29 13:03:14 +0100

    Adds Test flight name

---
+++ src/test/java/PlaneTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlaneTest {
+    Plane plane;
+
+    @Before
+    public void setUp(){
+        plane = new Plane(PlaneType.BOEING);
+    }
+
+    @Test
+    public void canGetPlainType(){
+        assertEquals(PlaneType.BOEING, plane.getType());
+
+    }
+}
